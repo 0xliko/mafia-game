ca68d22d3fdcbfd9f7ae654cb1da52a3edb4e359
jsgrowing315 <jsgrowing315@outlook.com>
2021-12-15 10:36:42 +0800

    wallet connection integration without iframe

--- wallet-connect/Navigation.tsx
+++ wallet-connect/Navigation.tsx
@@ -1,91 +1,28 @@
 import { useWallet } from '@solana/wallet-adapter-react';
-import {useWalletModal, WalletDisconnectButton, WalletMultiButton} from '@solana/wallet-adapter-react-ui';
+import { useWalletModal } from '@solana/wallet-adapter-react-ui';
+import React, { FC,useMemo,useEffect } from 'react';
 
-import React, {FC, ReactNode, useCallback, useEffect, useMemo, useRef, useState} from 'react';
-export interface NavigationProps {
-    modalActivated: (url:string)=>{}
-}
-const Navigation: FC<NavigationProps> = ({modalActivated}) => {
-    const { publicKey, wallet, disconnect,connect,connecting,connected,ready } = useWallet();
-    const { setVisible,visible } = useWalletModal();
-    const [ parentUrl,setParentUrl ] = useState('');
-    const [walletEvent, setWalletEvent] = useState(false);
+
+const Navigation: FC = () => {
+    const { wallet,publicKey,disconnect} = useWallet();
+    const { setVisible } = useWalletModal();
     const base58 = useMemo(() => publicKey?.toBase58(), [publicKey]);
-    // @ts-ignore // for development
-    window.disconnect = disconnect;
-    // @ts-ignore // for development
-    window.connect = connect;
-
+    const connectWallet = ()=>{
+         setVisible(true)
+    }
     useEffect(()=>{
 
-        window.addEventListener('message',(e:MessageEvent)=>{
-            const {type} = e.data;
-            if(type=='openWalletModal'){
-                modalActivated(e.data.curUrl);
-                console.log("out event got__")
-                setWalletEvent(true);
-                setParentUrl(e.data.curUrl);
-            }
-        });
-
-    },[]);
-
-    useEffect(()=>{
-
-        if(walletEvent==true){
-            if(!wallet) {
-                setVisible(true);
-            }
-            else{
-                /// TODO
-                console.log("Already wallet connected")
-                if(connected){
-                    if(parentUrl) {
-                        window.parent.postMessage({type: 'walletConnected', address: base58,walletName:wallet?.name,walletIcon:wallet?.icon}, parentUrl);
-                    }
-                } else{
-                    if(!connecting) connect();
-                }
-            }
-            setWalletEvent(false);
+        if(base58) {
+            console.log(base58,"this is connected wallet address");
+            if(window.fetchNFTS)
+                window.fetchNFTS({address:base58})
+            disconnect();
         }
-    },[walletEvent])
-
-
-    useEffect(()=>{
-         if(!visible && !connecting) {
-             if(parentUrl!='') {
-                 window.parent.postMessage({type: 'closeDialog',}, parentUrl);
-             }
-         }
-    },[visible]);
-    useEffect(()=>{
-        if(connected){
-            if(parentUrl) {
-                window.parent.postMessage({type: 'walletConnected', address: base58,walletName:wallet?.name,walletIcon:wallet?.icon}, parentUrl);
-            }
-        } else{
-
-            if(parentUrl) {
-                console.log(publicKey,parentUrl,wallet,"disconnected & parentUrl ready")
-                window.parent.postMessage({type: 'walletDisconnected', address: base58,walletName:wallet?.name,walletIcon:wallet?.icon}, parentUrl);
-            }
-        }
-
-    },[connected,parentUrl])
-    useEffect(()=>{
-        if(wallet) {
-            connect();
-        }
-    },[wallet])
+    },[base58])
     return (
-        <nav>
-
-            <div>
-                <WalletMultiButton/>
-                {wallet && <WalletDisconnectButton />}
-            </div>
-        </nav>
+        <>
+            <label onClick={connectWallet}>Wallet Connect</label>
+        </>
     );
 };
 
--- wallet-connect/index.tsx
+++ wallet-connect/index.tsx
@@ -2,6 +2,7 @@ import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import Wallet from './Wallet';
 
+let container = document.querySelector('.wallet-connect');
 // Use require instead of import, and order matters
 require('@solana/wallet-adapter-react-ui/styles.css');
 require('./index.css');
@@ -10,5 +11,5 @@ ReactDOM.render(
     <StrictMode>
         <Wallet />
     </StrictMode>,
-    document.getElementById('root')
+    container
 );
